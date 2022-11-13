4bba13e90539cbbc3cf98990e7f5a6a0720a2d99
stojcevic13 <nemanjastojcevic107@gmail.com>
2022-11-13 10:56:56 +0100

    Implemented fill data in DriverAccountActivity

--- EasyGO2/app/src/main/java/com/example/easygo/driver/DriverAccountActivity.java
+++ EasyGO2/app/src/main/java/com/example/easygo/driver/DriverAccountActivity.java
@@ -8,8 +8,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.easygo.LoggedIn;
 import com.example.easygo.R;
+import com.example.easygo.model.users.Driver;
 import com.example.easygo.passenger.PassengerAccountActivity;
 import com.example.easygo.passenger.PassengerInboxActivity;
 import com.example.easygo.passenger.PassengerMainActivity;
@@ -17,12 +21,23 @@ import com.example.easygo.passenger.PassengerRideHistoryActivity;
 
 public class DriverAccountActivity extends AppCompatActivity {
 
+    private TextView txtUser;
+    private TextView txtEmail;
+    private TextView txtPhone;
+    private TextView txtAddress;
+    private ImageView profileImg;
+
+    private Driver driver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_account);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        this.driver = LoggedIn.getDriver();
+        setDriverData();
     }
 
     @Override
@@ -55,6 +70,18 @@ public class DriverAccountActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    private void setDriverData() {
+        String user = driver.getName() + " " + driver.getSurname();
+
+        ((TextView) findViewById(R.id.txtUser)).setText(user);
+        ((TextView) findViewById(R.id.txtEmail)).setText(driver.getEmail());
+        ((TextView) findViewById(R.id.txtPhone)).setText(driver.getPhone());
+        ((TextView) findViewById(R.id.txtAddress)).setText(driver.getAddress());
+
+        if (driver.getProfilePic() != -1)
+            ((ImageView) findViewById(R.id.profileImg)).setImageResource(driver.getProfilePic());
     }
 
 }
---
+++ EasyGO2/app/src/main/java/com/example/easygo/driver/DriverProfileActivity.java
@@ -0,0 +1,21 @@
+package com.example.easygo.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.easygo.R;
+
+public class DriverProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_profile);
+
+    }
+
+
+}
