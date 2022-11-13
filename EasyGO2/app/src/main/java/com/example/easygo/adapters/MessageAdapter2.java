824b320784483cb6204ac6a0aed044dbdecd934f
Anastasija Draskovic <99082640+AngieDraskovic@users.noreply.github.com>
2022-11-13 18:50:53 +0100

    Tried implementing adapters

---
+++ EasyGO2/app/src/main/java/com/example/easygo/adapters/MessageAdapter2.java
@@ -0,0 +1,63 @@
+package com.example.easygo.adapters;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.easygo.R;
+import com.example.easygo.mockup.MockupMessages;
+import com.example.easygo.model.Message;
+
+import org.w3c.dom.Text;
+
+public class MessageAdapter2 extends BaseAdapter
+{
+    private Activity activity;
+
+    public MessageAdapter2(Activity activity){
+
+        this.activity = activity;
+    }
+
+    @Override
+    public int getCount(){
+        return MockupMessages.getMessages().size();
+
+    }
+
+    @Override
+    public Message getItem(int index){
+        return MockupMessages.getMessages().get(index);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi = convertView;
+        Message message = MockupMessages.getMessages().get(position);
+
+        if(convertView==null)
+            vi = activity.getLayoutInflater().inflate(R.layout.item_container_messagge, null);
+
+        TextView name = (TextView)vi.findViewById(R.id.textName);
+        TextView email = (TextView)vi.findViewById(R.id.textEmail);
+        ImageView image = (ImageView)vi.findViewById(R.id.imageProfile);
+
+
+        name.setText(message.getDeliverer().getEmail());
+        email.setText(message.getText());
+
+        image.setImageResource(-1);
+        return  vi;
+
+
+
+    }
+}
--- EasyGO2/app/src/main/java/com/example/easygo/passenger/PassengerInboxActivity.java
+++ EasyGO2/app/src/main/java/com/example/easygo/passenger/PassengerInboxActivity.java
@@ -3,6 +3,7 @@ package com.example.easygo.passenger;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,15 +13,22 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.easygo.R;
+import com.example.easygo.adapters.MessageAdapter2;
 
 public class PassengerInboxActivity extends AppCompatActivity {
 
+
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_inbox);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.mesaggesRecyclerView);
+        MessageAdapter2 messageAdapter2 = new MessageAdapter2(this);
+        recyclerView.setAdapter(messageAdapter2);
     }
 
     @Override
